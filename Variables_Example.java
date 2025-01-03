ee76e48a1e250bf5eb60f0841237e391e9bcae4d
Amrit Kashyap <108220434+Amrit152004@users.noreply.github.com>
2025-01-03 09:13:42 +0530

    Add files via upload

---
+++ Variables_Example.java
@@ -0,0 +1,25 @@
+package task1;
+
+public class Variables_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		classname:Variables_Example 
+//		variableNames:
+//					number1,number2,result
+//		i)Sample Input :number1=12,number2=13	
+//		  Sample Output:number1 and number2 values are not equal 
+//	
+//		ii)Sample Input :number1=100,number2=100
+//		   Sample Output:number1 and number2 values are  equal 
+		int  number1=12;
+		int number2=12;
+		boolean result=number1==number2;
+		if(result!=true) {
+			System.out.println("Value 1 and Value 2 both are not equals");
+		}else {
+		System.out.println("Both are equals value");
+		}
+	}
+
+}
